c97bb7b418514b6d7ca2fc3ee75c2e3b9b0bc33f
pateljay2114 <jay81978@gmail.com>
2023-09-14 15:43:27 +0530

    done

--- MYSQL/ASSIGNMENT/assignment_1.sql
+++ MYSQL/ASSIGNMENT/assignment_1.sql
@@ -1,6 +1,11 @@
 create database HR ;
 use HR ;
 
+
+alter table emp_data add column location varchar(30) ;
+
+alter table emp_data add column DEPARTMENT_NAME varchar(22) ;
+
 select * from emp_data ;
 
 -- Q1 :  Display all information in the tables EMP and DEPT.
@@ -179,7 +184,228 @@ FROM emp_data
 order by EMPLOYEE_ID;
 
 
+/* PART - 3 */
 
+
+-- Q1 : 1. Write a query that displays the employee's last names only from the string's 2-5th position with 
+--         the first letter capitalized and all other letters lowercase, Give each column an appropriate label. 
+
+
+
+
+
+-- Q2 : Write a query that displays the employee's first name and last name along with a " in between for
+--  	e.g.: first name : Ram; last name : Kumar then Ram-Kumar. Also displays the month on which the employee has joined.
+
+SELECT
+  CONCAT(FIRST_NAME, '-', LAST_NAME) AS full_name,
+  MONTH(HIRE_DATE) AS join_month
+FROM emp_data ;
+
+-- Q3 : Write a query to display the employee's last name and if half of the salary is greater than 
+--      ten thousand then increase the salary by 10% else by 11.5% along with the bonus amount of 
+--      1500 each. Provide each column an appropriate label.
+
+SELECT
+    LAST_NAME ,
+    CASE
+        WHEN SALARY * 0.5 > 10000 THEN SALARY * 1.1
+        ELSE SALARY * 1.115
+    END AS "New Salary",
+    1500 AS "Bonus Amount"
+FROM
+    emp_data ;
+ 
+
+-- Q4 : Display the employee ID by Appending two zeros after 2nd digit and 'E' in the end, department id, 
+--      salary and the manager name all in Upper case, if the Manager name consists of 'z' replace it with '$! 
+
+SELECT 
+    CONCAT(
+        SUBSTRING(EMPLOYEE_ID, 1, 2), 
+        '00',                          
+        SUBSTRING(EMPLOYEE_ID, 3),     
+        'E'                           
+    ) AS modified_employee_id,
+    UPPER(DEPARTMENT_ID) AS modified_department_id,
+    SALARY,
+    REPLACE(UPPER(FIRST_NAME), 'Z', '$!') AS modified_manager_name
+FROM
+    emp_data;
+
+
+-- Q5 : Write a query that displays the employee's last names with the first letter capitalized and all other letters 
+--      lowercase, and the length of the names, for all employees whose name starts with J, A, or M. 
+--      Give each column an appropriate label. Sort the results by the employees' last names 
+
+
+SELECT 
+    CONCAT(
+        UPPER(LEFT(LAST_NAME, 1)), 
+        LOWER(SUBSTRING(LAST_NAME, 2))
+    ) AS "Employee Name",
+    LENGTH(LAST_NAME) AS "Name Length"
+FROM 
+    emp_data
+WHERE 
+    last_name LIKE 'J%' OR
+    last_name LIKE 'A%' OR
+    last_name LIKE 'M%' 
+ORDER BY 
+    last_name  ;
+    
+    
+    
+-- Q6 :  Create a query to display the last name and salary for all employees. Format the salary to 
+--       be 15 characters long, left-padded with $. Label the column SALARY 
+
+SELECT
+    last_name ,
+    LPAD(CONCAT('$', salary), 15, '$') AS "SALARY"
+FROM emp_data;
+
+
+-- Q7 :  Display the employee's name if it is a palindrome.
+
+SELECT FIRST_NAME
+FROM emp_data
+WHERE FIRST_NAME = REVERSE(FIRST_NAME);
+
+-- Q8 :  Display First names of all employees with initcaps.
+
+ 
+SELECT CONCAT(UPPER(SUBSTRING(FIRST_NAME, 1, 1)), LOWER(SUBSTRING(FIRST_NAME, 2))) AS initcaps_first_name
+FROM emp_data ;
+
+
+
+-- Q9 :  From LOCATIONS table, extract the word between first and second space from the STREET ADDRESS column.
+ 
+
+
+
+
+
+-- Q10 :  Extract first letter from First Name column and append it with the Last Name. Also add "@systechusa.com" at the end.
+--        Name the column as e-mail address. All characters should be in lower case. Display this along with their First Name.  
+
+SELECT
+    CONCAT(
+        LOWER(LEFT(FIRST_NAME, 1)), 
+        LOWER(LAST_NAME),
+        '@systechusa.com'
+    ) AS 'e-mail address' ,
+    FIRST_NAME
+FROM
+    emp_data ;
+    
+-- Q11 :  Display the names and job titles of all employees with the same job as Trenna.
+
+SELECT FIRST_NAME, JOB_ID
+FROM emp_data 
+WHERE JOB_ID = (SELECT JOB_ID FROM emp_data WHERE FIRST_NAME = 'JMALLIN');
+
+
+-- Q12 : Display the names and department name of all employees working in the same city as Trenna.    
+
+
+ 
+-- Q13 : Display the name of the employee whose salary is the lowest.    
+    
+SELECT FIRST_NAME,SALARY
+FROM emp_data
+ORDER BY SALARY
+LIMIT 1;
+
+-- Q14 : Display the names of all employees except the lowest paid.
+
+
+SELECT FIRST_NAME,SALARY
+FROM emp_data
+WHERE SALARY > (
+  SELECT MIN(SALARY)
+  FROM emp_data
+);
+
+
+
+
+/* Part : 4 */
+
+
+
+-- Q1 : Write a query to display the last name, department number, department name for all employees.
+
+select LAST_NAME,DEPARTMENT_ID,DEPARTMENT_NAME from emp_data ;
+
+
+-- Q2 : Create a unique list of all jobs that are in department 4. Include the location of the department in the output. 
+
+SELECT DISTINCT DEPARTMENT_ID,
+                DEPARTMENT_NAME
+FROM emp_data ;
+
+
+-- Q3 : Write a query to display the employee last name,department name,location id and city of all employees who earn commission. 
+
+select LAST_NAME,DEPARTMENT_NAME,location from emp_data where COMMISSION_PCT is not null ;
+
+
+-- Q4 : Display the employee last name and department name of all employees who have an 'a' in their last name. 
+
+SELECT LAST_NAME, DEPARTMENT_NAME
+FROM emp_data
+WHERE LAST_NAME LIKE 'a%';
+
+
+-- Q5 :  Write a query to display the last name,job,department number and department name for all employees who work in Ahmedabad . 
+
+select LAST_NAME,DEPARTMENT_NAME,DEPARTMENT_ID from emp_data where location = 'Ahmedabad ' ;
+
+-- Q6 : Display the employee last name and employee number along with their manager's last name and manager number.
+
+SELECT EMPLOYEE_ID,LAST_NAME,MANAGER_ID
+FROM emp_data
+where  EMPLOYEE_ID = MANAGER_ID 
+order by MANAGER_ID  ;
+
+
+-- Q7 : Display the employee last name and employee number along with their manager's last 
+--        name and manager number (including the employees who have no manager). 
+
+
+-- Q8 : Create a query that displays employees last name,department number,and all the 
+--       employees who work in the same department as a given employee.
+
+
+select LAST_NAME, DEPARTMENT_ID ,DEPARTMENT_NAME from emp_data 
+order by  DEPARTMENT_NAME ;
+
+
+-- Q9 :  Create a query that displays the name,job,department name,salary,grade for all 
+--        employees. Derive grade based on salary(>=5000=A, >=3000=B,<3000=C).
+
+SELECT
+    FIRST_NAME,JOB_ID,DEPARTMENT_NAME,SALARY,
+
+    CASE
+        WHEN SALARY >= 5000 THEN 'A'
+        WHEN SALARY >= 3000 THEN 'B'
+        ELSE 'C'
+    END AS Grade
+FROM
+    emp_data ;
+    
+/*   Q10 : Display the names and hire date for all employees who were hired before their 
+		   managers along withe their manager names and hire date. Label the columns as Employee 
+           name, emp_hire_date,manager name,man_hire_date.   */
+           
+
+
+
+
+
+           
 
 
 
